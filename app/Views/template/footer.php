f3e1ed8f565a1d55bded126931617aabb782f1f5
MobileJC <jackiechan31590@gmail.com>
2025-06-06 11:20:23 +1000

    Completing header and footer
    Footer for author info

---
+++ app/Views/template/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="container">
+        <div class="row vcenter">
+            <div class="col-xs-6">
+				<!-- Commissioned by Wing -->
+                <p>&copy; 2025-<?php echo date("Y"); ?>Author: MobileJC</p>
+				<a href="https://github.com/MobileJC">My Github</a>
+				<a href="https://www.linkedin.com/in/wilkinson-john-chan-406772270/">My LinkedIn</a>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ app/Views/template/header.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <title>CellStocker</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+    <script src="<?php echo base_url(); ?>assets/js/jquery-3.6.0.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">CellStocker</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+
+</body>
+
+</html>
